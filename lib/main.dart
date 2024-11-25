d6574871970fc792d9b5f336b6c20628f07c007f
meshariSalhouli <mesharialhouli12@gmail.com>
2024-11-25 13:45:39 +0300

    login and register done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'package:adopt_app/pages/add_page.dart';
-import 'package:adopt_app/pages/home_page.dart';
-import 'package:adopt_app/pages/signin_page.dart';
-import 'package:adopt_app/pages/signup_page.dart';
-import 'package:adopt_app/pages/update_page.dart';
-import 'package:adopt_app/providers/auth_provider.dart';
-import 'package:adopt_app/providers/pets_provider.dart';
 import 'package:expressflutter_1/pages/home.dart';
 import 'package:expressflutter_1/pages/login.dart';
 import 'package:expressflutter_1/pages/register.dart';
+import 'package:expressflutter_1/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -16,7 +10,7 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider<PetsProvider>(create: (_) => PetsProvider()),
+        // ChangeNotifierProvider<PetsProvider>(create: (_) => PetsProvider()),
         ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
       ],
       child: MyApp(),
@@ -35,21 +29,18 @@ class MyApp extends StatelessWidget {
 
   final _router = GoRouter(
     routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const HomePage(),
-      ),
       // GoRoute(
-      //   path: '/add',
-      //   builder: (context, state) => AddPage*(),
+      //   path: '/',
+      //   builder: (context, state) => const HomePage(),
       // ),
+
       GoRoute(
         path: '/signup',
-        builder: (context, state) => SignupPage(),
+        builder: (context, state) => RegisterPage(),
       ),
       GoRoute(
         path: '/signin',
-        builder: (context, state) => SigninPage(),
+        builder: (context, state) => LoginPage(),
       ),
       // GoRoute(
       //   path: '/update/:recipeId',
